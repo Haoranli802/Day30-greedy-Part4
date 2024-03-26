078dacbf7df108b122415b8519b4c5f9a689bac0
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-03-26 09:15:23 +0800

    Create LC860.java

---
+++ LC860.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for(int i : bills){
+            if(i == 5) five ++;
+            else if(i == 10){
+                if(five < 1) return false;
+                five --;
+                ten ++;
+            }
+            else{
+                if(ten > 0){
+                    ten -- ;
+                    five --;
+                }
+                else five -= 3;
+                if(five < 0) return false;
+            }
+        }
+        return true;
+    }
+}
+//O(N), O(1)
